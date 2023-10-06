a85657339a1e3e0d37f8ee837438c8ba62c0cea0
suarez025 <heljapi110@gmail.com>
2023-10-06 17:41:55 -0500

    read

--- Js/index.js
+++ Js/index.js
@@ -4,6 +4,7 @@ function app() {
     let crud = new CRUD("ejemplo");
     crud.create([1, 2, 3]);
     crud.create({ message: "hola mundo" });
+    crud.read(2);
 
     let crud2 = new CRUD("ejemplo");
     console.log(crud2.readAll());
